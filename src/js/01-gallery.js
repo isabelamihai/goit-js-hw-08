ad81087ce183b4fee982da6596fab04eb2acdf49
isabelamihai <isabelaelenamihai@gmail.com>
2025-03-21 20:02:15 +0200

    Update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelectorAll(".gallery a");
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true, // pentru a arăta titlurile imaginilor
+  captionSelector: "img", // selectorul pentru a extrage titlul din imagine
+  captionType: "attr", // specificăm tipul de caption (de exemplu, "alt" text)
+  captionsData: "alt", // folosim textul din atributul "alt" al imaginii
+  captionPosition: "bottom", // poziția descrierii
+  overlayOpacity: 0.9, // opacitatea overlay-ului
+});
+
+
+
